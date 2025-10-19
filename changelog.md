18c5d5b987de857b6311abb4a09096a44bca03ee
axperty <kevingelhorn0@gmail.com>
2025-10-19 00:22:08 -0300

    See changelog.md for the latest changes.

--- changelog.md
+++ changelog.md
@@ -1,15 +1,18 @@
-<p align="left"><img src="https://cdn.modrinth.com/data/T8Fpxcl7/images/b082e8bc447ae9e446948c356bbdedd7a2bea422.png" alt="Too Many Paintings! Logo" width="400">
+![Too Many Paintings](https://cdn.modrinth.com/data/T8Fpxcl7/images/b082e8bc447ae9e446948c356bbdedd7a2bea422.png)
 
-<h3>Minor Update</h3>
-<p><b>Summary:</b> This update adds 2 new paintings from newer versions.</p>
-<hr/>
+***
 
-<h3>✨ What's New:</h3>
-<ul>
-  <li>Added 2 more paintings.</li>
-</ul>
+### ✨ What's New:
 
-<h3>🛠️ Bug Fixes</h3>
-<ul>
-  <li>Painting names are now displayed correctly.</li>
-</ul>
+- Added skin paintings from renibond, takeamuke, KarmaTTT, and Federico (takeamuke's cat).
+- Paintings have been enhanced and improved.
+
+### 🛠️ Minor Changes:
+
+- Removed "Cornfield", Cherry Mountains", "Frida", "Galaxy", "House", "Landscape", and "Mountains" paintings.
+
+***
+
+### ❤️ Support:
+
+- Support this mod by [creating your own server](https://url-shortener.curseforge.com/voMOm) or [donating via PayPal](https://paypal.me/kevgelhorn).
--- src/main/java/com/axperty/toomanypaintings/painting/ModPaintings.java
+++ src/main/java/com/axperty/toomanypaintings/painting/ModPaintings.java
@@ -16,25 +16,18 @@ public class ModPaintings {
     public static final PaintingVariant BEE = registerPainting("bee", new PaintingVariant(16, 16));
     public static final PaintingVariant BENDA = registerPainting("benda", new PaintingVariant(16, 16));
     public static final PaintingVariant CAFE = registerPainting("cafe", new PaintingVariant(16, 32));
-    public static final PaintingVariant CHERRY_MOUNTAINS = registerPainting("cherry_mountains", new PaintingVariant(32, 32));
     public static final PaintingVariant CLOCKS = registerPainting("clocks", new PaintingVariant(32, 16));
-    public static final PaintingVariant CORNFIELD = registerPainting("cornfield", new PaintingVariant(32, 32));
     public static final PaintingVariant COW = registerPainting("cow", new PaintingVariant(16, 16));
     public static final PaintingVariant CREATION = registerPainting("creation", new PaintingVariant(32, 16));
     public static final PaintingVariant DROWNED = registerPainting("drowned", new PaintingVariant(16, 16));
     public static final PaintingVariant EARTH = registerPainting("earth", new PaintingVariant(32, 32));
     public static final PaintingVariant ENDERMAN = registerPainting("enderman", new PaintingVariant(16, 16));
     public static final PaintingVariant FRENCH = registerPainting("french", new PaintingVariant(32, 16));
-    public static final PaintingVariant FRIDA = registerPainting("frida", new PaintingVariant(16, 32));
-    public static final PaintingVariant GALAXY = registerPainting("galaxy", new PaintingVariant(32, 32));
     public static final PaintingVariant GHAST = registerPainting("ghast", new PaintingVariant(16, 16));
     public static final PaintingVariant GIORGIO = registerPainting("giorgio", new PaintingVariant(32, 16));
-    public static final PaintingVariant HOUSE = registerPainting("house", new PaintingVariant(32, 32));
     public static final PaintingVariant LADY = registerPainting("lady", new PaintingVariant(16, 32));
-    public static final PaintingVariant LANDSCAPE = registerPainting("landscape", new PaintingVariant(32, 16));
     public static final PaintingVariant LISA = registerPainting("lisa", new PaintingVariant(16, 32));
     public static final PaintingVariant LOVERS = registerPainting("lovers", new PaintingVariant(32, 32));
-    public static final PaintingVariant MOUNTAINS = registerPainting("mountains", new PaintingVariant(32, 16));
     public static final PaintingVariant NIGHT = registerPainting("night", new PaintingVariant(32, 16));
     public static final PaintingVariant PIG = registerPainting("pig", new PaintingVariant(16, 16));
     public static final PaintingVariant PILLARS = registerPainting("pillars", new PaintingVariant(32, 32));
@@ -60,7 +53,6 @@ public class ModPaintings {
     public static final PaintingVariant ZOMBIE_PIGMAN = registerPainting("zombie_pigman", new PaintingVariant(16, 16));
 
     // New added paintings
-    // Version 1.0.1
     public static final PaintingVariant LONELY_TREE = registerPainting("lonely_tree", new PaintingVariant(32, 32));
     public static final PaintingVariant CHERRY_TREE = registerPainting("cherry_tree", new PaintingVariant(32, 32));
     public static final PaintingVariant WATERFALL = registerPainting("waterfall", new PaintingVariant(32, 32));
@@ -73,9 +65,14 @@ public class ModPaintings {
     public static final PaintingVariant CASTLE = registerPainting("castle", new PaintingVariant(32, 32));
 
     // New added paintings
-    // Version 1.0.2
     public static final PaintingVariant A_BOAT = registerPainting("a_boat", new PaintingVariant(32, 32));
     public static final PaintingVariant PYRAMID = registerPainting("pyramid", new PaintingVariant(32, 32));
+
+    // New added paintings
+    public static final PaintingVariant RENIBOND = registerPainting("renibond", new PaintingVariant(16, 16));
+    public static final PaintingVariant TAKEAMUKE = registerPainting("takeamuke", new PaintingVariant(16, 16));
+    public static final PaintingVariant KARMA = registerPainting("karma", new PaintingVariant(16, 16));
+    public static final PaintingVariant FEDERICO = registerPainting("federico", new PaintingVariant(32, 32));
 
     private static PaintingVariant registerPainting(String name, PaintingVariant paintingVariant) {
         return Registry.register(Registries.PAINTING_VARIANT, new Identifier(TooManyPaintings.MODID, name), paintingVariant);
