50bcd40abe1e692316eca4682c2be5d94095211a
axperty <107956734+axperty@users.noreply.github.com>
2026-08-17 14:42:59 -0300

    Initial commit.

--- src/main/java/com/axperty/toomanypaintings/TooManyPaintings.java
+++ src/main/java/com/axperty/toomanypaintings/TooManyPaintings.java
@@ -1,17 +1,22 @@
 package com.axperty.toomanypaintings;
 
-import com.axperty.toomanypaintings.painting.ModPaintings;
-import net.fabricmc.api.ModInitializer;
+import com.axperty.toomanypaintings.paintings.ModPaintings;
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-public class TooManyPaintings implements ModInitializer {
-    public static final String MODID = "toomanypaintings";
-    public static final Logger LOGGER = LoggerFactory.getLogger(MODID);
+@Mod(TooManyPaintings.MOD_ID)
+public class TooManyPaintings {
+    public static final String MOD_ID = "toomanypaintings";
+    public static final Logger LOGGER = LogUtils.getLogger();
 
-    @Override
-    public void onInitialize() {
-        ModPaintings.registerPaintings();
+    public TooManyPaintings() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        ModPaintings.register(modEventBus);
+        MinecraftForge.EVENT_BUS.register(this);
         System.out.println("Too Many Paintings! loaded");
     }
 }
