d71c3005201a8eb8d0e42f843d5a5cb8babb16a1
axperty <107956734+axperty@users.noreply.github.com>
2026-08-17 14:56:53 -0300

    Initial commit.

--- src/main/java/com/axperty/toomanypaintings/TooManyPaintings.java
+++ src/main/java/com/axperty/toomanypaintings/TooManyPaintings.java
@@ -5,8 +5,4 @@ import net.minecraftforge.fml.common.Mod;
 @Mod(TooManyPaintings.MOD_ID)
 public class TooManyPaintings {
     public static final String MOD_ID = "toomanypaintings";
-
-    public TooManyPaintings() {
-
-    }
 }
